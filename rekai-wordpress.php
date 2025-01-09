a09f57d5a13e5455fe8f579a0d71bd5ed5cde395
Maxemilian Grönblom <maxemiliang@gmail.com>
2025-01-09 10:19:45 +0200

    chore: initial commit 🎉

---
+++ rekai-wordpress.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Rekai WordPress
+ * Plugin URI: https://github.com/jco-digital/rekai-wordpress
+ * Description: Rek.ai integration for WordPress
+ * Version: 0.1.0
+ * Author: J&Co Digital Oy
+ * Author URI: https://jco.fi
+ * License: GPLv3
+ */
