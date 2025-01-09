7423ef3b3813de9524289eeec48e34f0c34431e7
Maxemilian Grönblom <maxemiliang@gmail.com>
2025-01-09 11:29:17 +0200

    chore: finalize zip build and makefile release. 🔧

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,122 @@
+const path = require("node:path");
+const fs = require("node:fs");
+
+/**
+ * Converts a glob pattern to a RegExp
+ * @param {string} glob - The glob pattern (e.g. "*.zip")
+ * @returns {RegExp} - Regular expression that matches the glob pattern
+ */
+function globToRegExp(glob) {
+    // Escape special regex characters except * and ?
+    const escaped = glob.replace(/[.+^${}()|[\]\\]/g, '\\$&');
+
+    // Convert glob wildcards to regex patterns
+    const pattern = escaped
+        .replace(/\*/g, '.*')     // * matches any number of characters
+        .replace(/\?/g, '.');     // ? matches exactly one character
+
+    // Create regex that matches the entire string
+    return new RegExp(`^${pattern}$`);
+}
+
+/**
+ * Tests if a filepath matches a glob pattern
+ * @param {string} filepath - The filepath to test
+ * @param {string} pattern - The glob pattern to match against
+ * @returns {boolean} - Whether the filepath matches the pattern
+ */
+function matchGlobPattern(filepath, pattern) {
+    const regex = globToRegExp(pattern);
+    return regex.test(filepath);
+}
+
 module.exports = function (grunt) {
-	"use strict";
+    "use strict";
 
-	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON("package.json"),
+    const distFile = fs.readFileSync(".distignore").toString()
+    const ignoredFiles = distFile.split("\n").map(line => line.trim().replaceAll("\r", "")).filter(line => line.length > 0 || line.startsWith("#"))
 
-		addtextdomain: {
-			options: {
-				textdomain: "rekai-wordpress",
-			},
-			update_all_domains: {
-				options: {
-					updateDomains: true,
-				},
-				src: [
-					"*.php",
-					"**/*.php",
-					"!.git/**/*",
-					"!bin/**/*",
-					"!node_modules/**/*",
-					"!tests/**/*",
-					"!vendor/**/*",
-					"!vendor-prefixed/**/*",
-				],
-			},
-		},
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
 
-		wp_readme_to_markdown: {
-			your_target: {
-				files: {
-					"README.md": "readme.txt",
-				},
-			},
-		},
+        addtextdomain: {
+            options: {
+                textdomain: "rekai-wordpress",
+            },
+            update_all_domains: {
+                options: {
+                    updateDomains: true,
+                },
+                src: [
+                    "*.php",
+                    "**/*.php",
+                    "!.git/**/*",
+                    "!bin/**/*",
+                    "!node_modules/**/*",
+                    "!tests/**/*",
+                    "!vendor/**/*",
+                    "!vendor-prefixed/**/*",
+                ],
+            },
+        },
 
-		makepot: {
-			target: {
-				options: {
-					domainPath: "/languages",
-					exclude: [
-						".git/*",
-						"bin/*",
-						"node_modules/*",
-						"tests/*",
-						"vendor/*",
-						"vendor-prefixed/*",
-					],
-					mainFile: "rekai-wordpress.php",
-					potFilename: "rekai-wordpress.pot",
-					potHeaders: {
-						poedit: true,
-						"x-poedit-keywordslist": true,
-					},
-					type: "wp-plugin",
-					updateTimestamp: true,
-				},
-			},
-		},
-	});
+        wp_readme_to_markdown: {
+            your_target: {
+                files: {
+                    "README.md": "readme.txt",
+                },
+            },
+        },
 
-	grunt.loadNpmTasks("grunt-wp-i18n");
-	grunt.loadNpmTasks("grunt-wp-readme-to-markdown");
-	grunt.registerTask("default", ["i18n", "readme"]);
-	grunt.registerTask("i18n", ["addtextdomain", "makepot"]);
-	grunt.registerTask("readme", ["wp_readme_to_markdown"]);
+        makepot: {
+            target: {
+                options: {
+                    domainPath: "/languages",
+                    exclude: [
+                        ".git/*",
+                        "bin/*",
+                        "node_modules/*",
+                        "tests/*",
+                        "vendor/*",
+                        "vendor-prefixed/*",
+                    ],
+                    mainFile: "rekai-wordpress.php",
+                    potFilename: "rekai-wordpress.pot",
+                    potHeaders: {
+                        poedit: true,
+                        "x-poedit-keywordslist": true,
+                    },
+                    type: "wp-plugin",
+                    updateTimestamp: true,
+                },
+            },
+        },
 
-	grunt.util.linefeed = "\n";
-};
+        zip: {
+            'skip-files': {
+                router: function (filepath) {
+                    const isIgnored = ignoredFiles.filter(f => {
+                        // Don't zip files that are ignored by .distignore also run a regex
+                        const match = matchGlobPattern(filepath, f)
+                        return filepath.startsWith(f) || filepath.includes(f) || match
+                    })
+                    if (isIgnored.length > 0) return null
+                    return filepath
+                },
+                src: ['**/*'],
+                dest: 'build/rekai-wordpress.zip'
+            },
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-wp-i18n");
+    grunt.loadNpmTasks("grunt-wp-readme-to-markdown");
+    grunt.loadNpmTasks("grunt-zip")
+    grunt.registerTask("default", ["i18n", "readme"]);
+    grunt.registerTask("release", ["i18n", "readme", "zip"])
+    grunt.registerTask("i18n", ["addtextdomain", "makepot"]);
+    grunt.registerTask("readme", ["wp_readme_to_markdown"]);
+
+    grunt.util.linefeed = "\n";
+}
--- Makefile
+++ Makefile
@@ -8,3 +8,9 @@ install:
 build: install
 	@echo "Building plugin assets"
 	@pnpm project:build
+
+.PHONY: build-release
+build-release: build
+	@echo "Bundling a release of the project"
+	@pnpm plugin:tasks
+	@pnpm plugin:zip
