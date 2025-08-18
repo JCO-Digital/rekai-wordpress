c56e92ca66ce3bd526f39131000f580c09abaf9f
Niklas Schönberg <niklas@uplink.fi>
2025-08-18 15:59:50 +0300

    Refactor: Use consistent text domain
    Use `rek-ai` consistently as the text domain.

--- rekai.php
+++ rekai.php
@@ -7,7 +7,7 @@
  * Author: J&Co Digital Oy
  * Author URI: https://jco.fi
  * Domain Path: /languages
- * Text Domain: rekai
+ * Text Domain: rek-ai
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
--- views/docs/docs.php
+++ views/docs/docs.php
@@ -1,7 +1,7 @@
 <?php // phpcs:ignore Squiz.Commenting.FileComment.Missing ?>
 <p>
-	<?php _e( '<a href="https://docs.rek.ai/integration-modules/wordpress" target="_blank">Documentation for the plugin</a>.', 'rekai' ); ?>
+	<?php _e( '<a href="https://docs.rek.ai/integration-modules/wordpress" target="_blank">Documentation for the plugin</a>.', 'rek-ai' ); ?>
 </p>
 <p>
-	<?php _e( '<a href="admin.php?page=rekai-shortcodes">Shortcode Generator</a>.', 'rekai' ); ?>
+	<?php _e( '<a href="admin.php?page=rekai-shortcodes">Shortcode Generator</a>.', 'rek-ai' ); ?>
 </p>
